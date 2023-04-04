1d5a1b4f7b9c5a6f29b74957a3576e180df09613
Balajiw920 <wbalaji2021@gmail.com>
2023-04-04 23:24:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+//const url = "https://jsonplaceholder.typicode.com/posts/1";
+
+// define the API URL
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// define the async function
+async function fetchData(callback) {
+  try {
+    // fetch data from the API
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // call the callback function with the data
+    callback(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// add event listener to the button
+document.querySelector('#btn').addEventListener('click', async () => {
+  // call the fetchData function and display the title in the output div
+  fetchData(data => {
+    const outputDiv = document.querySelector('#output');
+    outputDiv.textContent = data.title;
+  });
+});
+
 
 //your JS code here. If required.
